df748efd16de85d334ce4805182462995e59e020
ngn <jeliazkova.nina@gmail.com>
2012-01-12 12:19:38 +0200

    condition classes

---
+++ modbcum/modbcum-q/src/main/java/net/idea/modbcum/q/conditions/EQCondition.java
@@ -0,0 +1,41 @@
+package net.idea.modbcum.q.conditions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.idea.modbcum.i.IQueryCondition;
+
+public class EQCondition implements IQueryCondition {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6106481025033474986L;
+	private static List<EQCondition> instances= null;
+	private final static String eq = "=";
+	private EQCondition() {
+	}
+	public String getName() {
+		return eq;
+	}
+
+	public String getSQL() {
+		return eq;
+	}
+	public static EQCondition getInstance() {
+		List<EQCondition>  c = getAlowedValues();
+		return c.get(0);
+	}
+	public static List<EQCondition> getAlowedValues() {
+		if (instances == null) {
+			instances = new ArrayList<EQCondition>();
+			instances.add(new EQCondition());
+			return instances;
+		}
+		return instances;
+	}	
+	@Override
+	public String toString() {
+
+		return eq;
+	}
+}
