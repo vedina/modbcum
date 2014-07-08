26feaff6e6d0bc27f9ef8403cc3fe23970a29b8f
ngn <jeliazkova.nina@gmail.com>
2014-07-08 21:19:36 +0300

    exceptions moved to a separate package

---
+++ modbcum/modbcum-e/src/main/java/net/idea/modbcum/i/exceptions/AmbitException.java
@@ -0,0 +1,30 @@
+package net.idea.modbcum.i.exceptions;
+
+public class AmbitException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2708593901530215773L;
+
+	public AmbitException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public AmbitException(String arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AmbitException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public AmbitException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+}
