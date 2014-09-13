952bb54ec5cdfad29a471db72af4b3ab9c9f509a
ngn <jeliazkova.nina@gmail.com>
2014-09-13 09:19:42 +0300

    Customized query classes

---
+++ modbcum/modbcum-r/src/main/java/net/idea/modbcum/r/CliQueryReporter.java
@@ -0,0 +1,44 @@
+package net.idea.modbcum.r;
+
+import java.io.Writer;
+
+import net.idea.modbcum.i.IQueryRetrieval;
+
+public abstract class CliQueryReporter<T,Q extends IQueryRetrieval<T>> extends QueryReporter<T,Q,Writer> {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3500571619173311448L;
+	protected long started;
+	protected long completed;
+	protected String comma = null;
+	
+	public CliQueryReporter() {
+		super();
+	}
+	@Override
+	public Writer process(Q query) throws Exception {
+		started = System.currentTimeMillis();
+		return super.process(query);
+	};
+	
+	@Override
+	public void header(Writer output, Q query) {
+		
+	}
+
+	@Override
+	public void footer(Writer output, Q query) {
+		completed = System.currentTimeMillis()-started;
+	}
+
+	public void footerJSON(Writer writer) throws Exception {
+			writer.write(",\n\"retrieved_ms\":");
+			writer.write(Long.toString(completed));
+			writer.write("\n}\n");
+	}
+	
+	public long getTimeElapsed() {
+		return completed;
+	}
+}
