19e0c4297cf27523893f8a3374aed3a4e7802d0e
ngn <jeliazkova.nina@gmail.com>
2015-05-27 16:22:58 +0300

    named config arg

--- modbcum/modbcum-c/src/main/java/net/idea/modbcum/c/DataSourceC3P0.java
+++ modbcum/modbcum-c/src/main/java/net/idea/modbcum/c/DataSourceC3P0.java
@@ -14,46 +14,52 @@ public class DataSourceC3P0 implements IDataSourcePool {
 	return verbose;
     }
 
-    public DataSourceC3P0(String connectURI) throws Exception {
-	this(connectURI, "com.mysql.jdbc.Driver");
+    public DataSourceC3P0(String namedConfig, String connectURI) throws Exception {
+	this(namedConfig, connectURI, "com.mysql.jdbc.Driver");
     }
 
-    public DataSourceC3P0(String connectURI, String driverName) throws Exception {
-	this(connectURI, driverName, false);
+    public DataSourceC3P0(String namedConfig, String connectURI, String driverName) throws Exception {
+	this(namedConfig, connectURI, driverName, false);
     }
 
-    public DataSourceC3P0(String connectURI, String driverName, boolean verbose) throws Exception {
+    public DataSourceC3P0(String namedConfig, String connectURI, String driverName, boolean verbose) throws Exception {
 	Class.forName(driverName);
 	/*
 	 * http://www.mchange.com/projects/c3p0/index.html#using_c3p0
 	 */
-	datasource = new ComboPooledDataSource(); // create a new datasource
-						  // object
+	datasource = new ComboPooledDataSource(namedConfig); // create a new
+							     // datasource
+	// object
 	datasource.setJdbcUrl(connectURI);
+
 	if (verbose)
 	    datasource.setConnectionCustomizerClassName("net.idea.modbcum.c.VerboseConnectionCustomizer");
-	datasource.setMaxPoolSize(512);
 
-	/**
-	 * http://www.mchange.com/projects/c3p0/index.html#
-	 * configuring_connection_testing
-	 * 
-	 * datasource.setAutomaticTestTable("version");
-	 * datasource.setPreferredTestQuery("SELECT 1");
-	 * datasource.setTestConnectionOnCheckin(true);
-	 * datasource.setIdleConnectionTestPeriod(5);
-	 */
-	datasource.setTestConnectionOnCheckin(true);
-	// datasource.setIdleConnectionTestPeriod(3000);
-	datasource.setPreferredTestQuery("/*ping*/ SELECT 1");
-	// datasource.setMaxIdleTimeExcessConnections(2400); // should be less
-	// than
-	// setIdleConnectionTestPeriod
-	/**
-	 * http://www.mchange.com/projects/c3p0/#checkoutTimeout
-	 */
-	datasource.setCheckoutTimeout(600000);
-	datasource.setNumHelperThreads(10);
+	if (namedConfig == null) {
+	    datasource.setMaxPoolSize(512);
+
+	    /**
+	     * http://www.mchange.com/projects/c3p0/index.html#
+	     * configuring_connection_testing
+	     * 
+	     * datasource.setAutomaticTestTable("version");
+	     * datasource.setPreferredTestQuery("SELECT 1");
+	     * datasource.setTestConnectionOnCheckin(true);
+	     * datasource.setIdleConnectionTestPeriod(5);
+	     */
+	    datasource.setTestConnectionOnCheckin(true);
+	    // datasource.setIdleConnectionTestPeriod(3000);
+	    datasource.setPreferredTestQuery("/*ping*/ SELECT 1");
+	    // datasource.setMaxIdleTimeExcessConnections(2400); // should be
+	    // less
+	    // than
+	    // setIdleConnectionTestPeriod
+	    /**
+	     * http://www.mchange.com/projects/c3p0/#checkoutTimeout
+	     */
+	    datasource.setCheckoutTimeout(600000);
+	    datasource.setNumHelperThreads(10);
+	}
     }
 
     public void close() throws Exception {
