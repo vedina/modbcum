cc88c2ca06741b6ef36cb10e20c3206ec49619dc
vedina <jeliazkova.nina@gmail.com>
2020-01-29 10:26:32 +0200

    methods to check condition parameter validity

--- modbcum/modbcum-q/src/main/java/net/idea/modbcum/q/conditions/StringCondition.java
+++ modbcum/modbcum-q/src/main/java/net/idea/modbcum/q/conditions/StringCondition.java
@@ -2,184 +2,206 @@ package net.idea.modbcum.q.conditions;
 
 import java.util.Hashtable;
 import java.util.Map;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
 
 import net.idea.modbcum.i.IQueryCondition;
 
 //todo refactor with enums
 public class StringCondition implements IQueryCondition {
-    /**
-     * 
-     */
-    private static final long serialVersionUID = -517113006900728995L;
-    private static Map<String, StringCondition> instances = null;
-    public static final String C_EQ = "=";
-    public static final String C_NOTEQ = "!=";
-    public static final String C_LIKE = "like";
-    public static final String C_NOTLIKE = "not like";
-    public static final String C_SOUNDSLIKE = "sounds like";
-    public static final String C_REGEXP = "regexp";
-    public static final String C_STARTS_WITH = "regexp ^";
-    public static final String C_ISNULL = "is null";
-    public static final String C_ISNOTNULL = "is not null";
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -517113006900728995L;
+	private static Map<String, StringCondition> instances = null;
+	public static final String C_EQ = "=";
+	public static final String C_NOTEQ = "!=";
+	public static final String C_LIKE = "like";
+	public static final String C_NOTLIKE = "not like";
+	public static final String C_SOUNDSLIKE = "sounds like";
+	public static final String C_REGEXP = "regexp";
+	public static final String C_STARTS_WITH = "regexp ^";
+	public static final String C_ISNULL = "is null";
+	public static final String C_ISNOTNULL = "is not null";
 
-    public enum STRING_CONDITION {
-	S_EQ {
-	    @Override
-	    public String toString() {
-		return StringCondition.C_EQ;
-	    }
+	public enum STRING_CONDITION {
+		S_EQ {
+			@Override
+			public String toString() {
+				return StringCondition.C_EQ;
+			}
 
-	    @Override
-	    public String getSQL() {
-		return StringCondition.C_EQ;
-	    }
-	},
-	S_NOTEQ {
-	    @Override
-	    public String toString() {
-		return StringCondition.C_NOTEQ;
-	    }
+			@Override
+			public String getSQL() {
+				return StringCondition.C_EQ;
+			}
+		},
+		S_NOTEQ {
+			@Override
+			public String toString() {
+				return StringCondition.C_NOTEQ;
+			}
 
-	    @Override
-	    public String getSQL() {
-		return StringCondition.C_NOTEQ;
-	    }
-	},
-	S_LIKE {
-	    @Override
-	    public String toString() {
-		return StringCondition.C_LIKE;
-	    }
+			@Override
+			public String getSQL() {
+				return StringCondition.C_NOTEQ;
+			}
+		},
+		S_LIKE {
+			@Override
+			public String toString() {
+				return StringCondition.C_LIKE;
+			}
 
-	    @Override
-	    public String getSQL() {
-		return StringCondition.C_LIKE;
-	    }
-	},
-	S_NOTLIKE {
-	    @Override
-	    public String toString() {
-		return StringCondition.C_NOTLIKE;
-	    }
+			@Override
+			public String getSQL() {
+				return StringCondition.C_LIKE;
+			}
+		},
+		S_NOTLIKE {
+			@Override
+			public String toString() {
+				return StringCondition.C_NOTLIKE;
+			}
 
-	    @Override
-	    public String getSQL() {
-		return StringCondition.C_NOTLIKE;
-	    }
-	},
-	S_SOUNDSLIKE {
-	    @Override
-	    public String toString() {
-		return StringCondition.C_SOUNDSLIKE;
-	    }
+			@Override
+			public String getSQL() {
+				return StringCondition.C_NOTLIKE;
+			}
+		},
+		S_SOUNDSLIKE {
+			@Override
+			public String toString() {
+				return StringCondition.C_SOUNDSLIKE;
+			}
 
-	    @Override
-	    public String getSQL() {
-		return StringCondition.C_SOUNDSLIKE;
-	    }
-	},
-	S_REGEXP {
-	    @Override
-	    public String toString() {
-		return "regular expression";
-	    }
+			@Override
+			public String getSQL() {
+				return StringCondition.C_SOUNDSLIKE;
+			}
+		},
+		S_REGEXP {
+			@Override
+			public String toString() {
+				return "regular expression";
+			}
 
-	    @Override
-	    public String getSQL() {
-		return StringCondition.C_REGEXP;
-	    }
-	},
-	S_STARTS_WITH {
-	    @Override
-	    public String toString() {
-		return "starts with";
-	    }
+			@Override
+			public String getSQL() {
+				return StringCondition.C_REGEXP;
+			}
+			@Override
+			public boolean acceptValue(String value) {
+				try {
+					Pattern.compile(getParam(value));
+					return true;
+				} catch (PatternSyntaxException x) {
+					return false;
+				}
+			}
+		},
+		S_STARTS_WITH {
+			@Override
+			public String toString() {
+				return "starts with";
+			}
 
-	    @Override
-	    public String getSQL() {
-		return StringCondition.C_REGEXP;
-	    }
+			@Override
+			public String getSQL() {
+				return StringCondition.C_REGEXP;
+			}
+			@Override
+			public boolean acceptValue(String value) {
+				try {
+					Pattern.compile(getParam(value));
+					return true;
+				} catch (PatternSyntaxException x) {
+					return false;
+				}
+			}
+			@Override
+			public String getParam(String value) {
+				if (value == null)
+					return value;
+				return '^' + value;
+			}
 
-	    @Override
-	    public String getParam(String value) {
-		if (value == null)
-		    return value;
-		return '^' + value;
-	    }
+			@Override
+			public String getName() {
+				return StringCondition.C_STARTS_WITH;
+			}
+		},
+		S_ISNOTNULL {
+			@Override
+			public String getSQL() {
+				return StringCondition.C_ISNOTNULL;
+			}
 
-	    @Override
-	    public String getName() {
-		return StringCondition.C_STARTS_WITH;
-	    }
-	},
-	S_ISNOTNULL {
-	    @Override
-	    public String getSQL() {
-		return StringCondition.C_ISNOTNULL;
-	    }
+		},
+		S_ISNULL {
+			@Override
+			public String getSQL() {
+				return StringCondition.C_ISNULL;
+			}
 
-	},
-	S_ISNULL {
-	    @Override
-	    public String getSQL() {
-		return StringCondition.C_ISNULL;
-	    }
+		};
 
-	};
+		public String getName() {
+			return getSQL();
+		}
 
-	public String getName() {
-	    return getSQL();
+		public abstract String getSQL();
+		public boolean acceptValue(String value) {
+			return true;
+		}
+		public String getParam(String value) {
+			return value;
+		}
+
 	}
 
-	public abstract String getSQL();
+	protected STRING_CONDITION value;
+
+	public StringCondition(STRING_CONDITION value) {
+		this.value = value;
+	}
 
 	public String getParam(String value) {
-	    return value;
+		return this.value.getParam(value);
 	}
-    }
 
-    protected STRING_CONDITION value;
-
-    public StringCondition(STRING_CONDITION value) {
-	this.value = value;
-    }
-
-    public String getParam(String value) {
-	return this.value.getParam(value);
-    }
-
-    public String getName() {
-	return value.toString();
-    }
-
-    public String getSQL() {
-	return value.getSQL();
-    }
-
-    public static Map<String, StringCondition> getAlowedValues() {
-	if (instances == null) {
-	    Map<String, StringCondition> list = new Hashtable<String, StringCondition>();
-	    for (STRING_CONDITION c : STRING_CONDITION.values())
-		list.put(c.getName(), new StringCondition(c));
-	    instances = list;
+	public String getName() {
+		return value.toString();
 	}
-	return instances;
-    }
 
-    public static StringCondition getInstance(String condition) {
-	try {
-	    StringCondition c = getAlowedValues().get(condition);
-	    if (c != null)
-		return c;
-	} catch (Exception x) {
-
+	public String getSQL() {
+		return value.getSQL();
 	}
-	return getAlowedValues().get(C_EQ);
-    }
 
-    @Override
-    public String toString() {
-	return getSQL();
-    }
+	public static Map<String, StringCondition> getAlowedValues() {
+		if (instances == null) {
+			Map<String, StringCondition> list = new Hashtable<String, StringCondition>();
+			for (STRING_CONDITION c : STRING_CONDITION.values())
+				list.put(c.getName(), new StringCondition(c));
+			instances = list;
+		}
+		return instances;
+	}
+
+	public static StringCondition getInstance(String condition) {
+		try {
+			StringCondition c = getAlowedValues().get(condition);
+			if (c != null)
+				return c;
+		} catch (Exception x) {
+
+		}
+		return getAlowedValues().get(C_EQ);
+	}
+
+	@Override
+	public String toString() {
+		return getSQL();
+	}
 
 }
